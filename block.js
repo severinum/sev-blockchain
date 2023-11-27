e0435fd8be03df5f1a18f17e915f6b480f10e85c
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-11-27 15:49:33 +0000

    First commit - Block skeleton

---
+++ block.js
@@ -0,0 +1,39 @@
+const SHA256 = require ('crypto-js/sha256')
+
+class Block {
+    
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp
+        this.lastHash = lastHash
+        this.hash = hash
+        this.data = data
+    }
+
+    static genesis() {
+        return new this('Genesis time', '------', '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9', [])
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now()
+        const lastHash = lastBlock.hash
+        const hash = Block.hash(timestamp, lastHash, data)
+
+        return new this(timestamp, lastHash, hash, data)
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString()
+    }
+
+
+    toString() {
+        return `Block - 
+            Timestamp   : ${this.timestamp}
+            Last Hash   : ${this.lastHash.substring(0, 20)}
+            Hash        : ${this.hash.substring(0, 20)}  
+            Data:       : ${this.data}
+        `
+    }
+}
+
+module.exports = Block;
