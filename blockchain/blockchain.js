9e500ee68ee925a160220c5f6514a2b3949562bc
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-12-20 16:06:47 +0000

    Remove extra logs

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,3 +1,4 @@
+const { log, LogsColours } = require('../utils/colours');
 const Block = require('./block');
 
 class Blockchain {
@@ -15,7 +16,7 @@ class Blockchain {
 
     isValidChain(chain) {
         if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())){
-            console.log('invalid genesis block in isValidChain')
+            log('invalid genesis block in isValidChain')
             return false
         }
 
@@ -24,18 +25,18 @@ class Blockchain {
             const lastBlock = chain[i-1]
 
             if(block.lastHash !== lastBlock.hash) {
-                //console.log(`block.lastHash !== lastBlock.hash ----- hashes doesn't match !`)
+                //log(`block.lastHash !== lastBlock.hash ----- hashes doesn't match !`)
                 return false
             }
 
             if(block.hash !== Block.blockHash(block)) {
-                console.log(`block.hash !== Block.blockHash(block) ---- doesn't match !`);
-                console.log(`--------------------------`)
-                console.log(`a: ${block.hash}`);
-                console.log(`b: ${Block.blockHash(block)}`);
-                console.log(`--------------------------`)
+                log(`block.hash !== Block.blockHash(block) ---- doesn't match !`);
+                log(`--------------------------`)
+                log(`a: ${block.hash}`);
+                log(`b: ${Block.blockHash(block)}`);
+                log(`--------------------------`)
             } else {
-                console.log(`block.hash == Block.blockHash(block) ---- match !!!!`);
+                log(`block.hash == Block.blockHash(block) ---- match !!!!`);
             }
 
             if(block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block) ){
@@ -47,53 +48,53 @@ class Blockchain {
     }
 
     replaceChain(newChain) {
-        console.log(`Started chain replacement process at: ${Date.now()}`)
+        log(`Started chain replacement process at: ${Date.now()}`)
         // check if new chain is longer (longer is a valid chain [our consesus])
         if(newChain.length <= this.chain.length) {
-            console.log('Received chain is not longer than the current chain [consenus matter]')
-            console.log(`Finished chain replacement process at: ${Date.now()}`)
+            log('Received chain is not longer than the current chain [consenus matter]')
+            log(`Finished chain replacement process at: ${Date.now()}`)
             return
         } else if (!this.isValidChain(newChain)) {
-            console.log('Received chain is not valid')
-            console.log(`Finished chain replacement process at: ${Date.now()}`)
+            log('Received chain is not valid')
+            log(`Finished chain replacement process at: ${Date.now()}`)
             return
         }
 
         // Original chain last block hash must be equal to new candidate chain length-1 block hash
         if(newChain[newChain.length - 2].hash != this.chain[this.chain.length -1].hash) {
-            console.log('FAIL!!!: Block is invalid. Last block hash in chain is not equal to new chain block hash')
+            log('FAIL!!!: Block is invalid. Last block hash in chain is not equal to new chain block hash', LogsColours.FgRed)
             // TODO: In case if new blockchain node is created/joned , its chain has length is 1 (genesis block only)
             // or node was inactive (connection issue). Then its chain length may be different. 
-            console.log('New node or inactive node rejoining chain')
+            log('New node or inactive node rejoining chain')
            
             for( let i= this.chain.length; i<newChain.length; i++) {
                 this.chain.push(newChain[i])
             }
-            console.log(`Chain synchronised`)
-            console.log(`newChain length=${newChain.length}, chain lenghth=${this.chain.length}`)
+            log(`Chain synchronised`)
+            log(`newChain length=${newChain.length}, chain lenghth=${this.chain.length}`)
             return
         } else {
-            console.log('OK. Chains hashes are correct')
+            log('OK. Chains hashes are correct')
         }
 
         // Original chain last block blockNumberInChain must be equal to new candidate chain length-1 block blockNumberInChain
         if(newChain[newChain.length - 2].blockNumberInChain != this.chain[this.chain.length -1].blockNumberInChain) {
-            console.log('FAIL!!! : Block is invalid. Last block number in chain is not equal to new chain block number')
+            log('FAIL!!! : Block is invalid. Last block number in chain is not equal to new chain block number')
             return
         } else {
-            console.log('OK. Chains block numbers are correct')
+           log('OK. Chains block numbers are correct')
         }
         
         // Find place where blocks have same length
         // Attach missing blocks 
         const differenceInChainLengths = newChain.length - this.chain.length
-        console.log(`Replacing chain strats at: ${Date.now()}`)
+        log(`Replacing chain strats at: ${Date.now()}`)
         this.chain.push(newChain[this.chain.length]) // updating chain
-        console.log(`Finished chain replacement process at: ${Date.now()}`)
+        log(`Finished chain replacement process at: ${Date.now()}`)
     }
 
     static findTransaction(blockchain, transactionId) {
-        console.log(`Searching for transaction with id: ${transactionId} `)
+        log(`Searching for transaction with id: ${transactionId} `)
         const blockchainLength  = blockchain.chain.length
         // TODO: find efficient way to find transactions
         // Blockchain will be too long to do the regualr search
